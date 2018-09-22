cc6b0d6e5d24108d6934058d18b31a2e5d3d5942
Yonekura <reverse.engau@gmail.com>
2018-09-22 19:01:27 +0900

    read tokens

--- token/token.go
+++ token/token.go
@@ -1,28 +1,42 @@
 package token
 
-type TokenType struct{
+type TokenType string
+
+type Token struct {
 	Type    TokenType
 	Literal string
 }
 
-const{
-	ILLEGAL   = "ILLEGAL"
-	EOF       = "EOF"
+const (
+	ILLEGAL = "ILLEGAL"
+	EOF     = "EOF"
 
-	IDENT     = "IDENT"
-	INT       = "INT"
+	IDENT = "IDENT" // add, forbar, x, y..
+	INT   = "INT"
 
-	ASSIGN    = "="
-	PLUS      = "+"
+	ASSIGN = "="
+	PLUS   = "+"
 
 	COMMA     = ","
 	SEMICOLON = ";"
 
-	LPAREN    = "("
-	RPAREN    = ")"
-	LBRACE    = "{"
-	RBRACE    = "}"
+	LPAREN = "("
+	RPAREN = ")"
+	LBRACE = "{"
+	RBRACE = "}"
 
-	FUNCTION  = "FUNCTION"
-	LET       = "LET"
+	FUNCTION = "FUNCTION"
+	LET      = "LET"
+)
+
+var keywords = map[string]TokenType{
+	"fn":  FUNCTION,
+	"let": LET,
+}
+
+func LookupIdent(ident string) TokenType {
+	if tok, ok := keywords[ident]; ok {
+		return tok
+	}
+	return IDENT
 }
