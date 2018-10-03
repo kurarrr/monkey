c11a97cefe6bd3bea770bead6eefae7407b5682b
Yonekura <reverse.engau@gmail.com>
2018-10-03 23:35:27 +0900

    add IDENT

--- token/token.go
+++ token/token.go
@@ -33,11 +33,26 @@ const (
 	SLASH    = "/"
 	LT       = "<"
 	GT       = ">"
+
+	EQ     = "=="
+	NOT_EQ = "!="
+
+	// Keywords
+	TRUE   = "TRUE"
+	FALSE  = "FALSE"
+	IF     = "IF"
+	ELSE   = "ELSE"
+	RETURN = "RETURN"
 )
 
 var keywords = map[string]TokenType{
-	"fn":  FUNCTION,
-	"let": LET,
+	"fn":     FUNCTION,
+	"let":    LET,
+	"true":   TRUE,
+	"false":  FALSE,
+	"if":     IF,
+	"else":   ELSE,
+	"return": RETURN,
 }
 
 func LookupIdent(ident string) TokenType {
